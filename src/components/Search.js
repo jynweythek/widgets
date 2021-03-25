6052426ede9288f4c3493ec0d8ff4a66b0eb77d0
Rustam Talapov <rustam_talapov@epam.com>
2021-03-25 20:12:10 +0200

    First useEffect makes 'debounced dependency' for the second one

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
-  console.log(results);
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -15,27 +24,15 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
+          srsearch: debouncedTerm
         }
       });
 
       setResults(data.query.search);
     };
-    let timeoutId;
-    if (term && !results.length) {
-      search();
-    } else {
-      timeoutId = setTimeout(() => {
-       if (term) {
-         search();
-       }
-      }, 750);
-    }
 
-    return () => {
-      clearTimeout(timeoutId);
-    }
-  }, [term, results.length]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map(result => {
     return (
