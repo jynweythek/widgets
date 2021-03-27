b2c4c22111b596029704558391e2f96bfa67ff88
Rustam Talapov <rustam_talapov@epam.com>
2021-03-27 17:04:58 +0200

    Translate component creation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 // import Accordion from "./components/Accordion";
 // import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from './components/Translate';
 
 // const items = [
 //   {title: 'What is React?', content: 'React is a front end javascript framework'},
@@ -16,19 +17,20 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  // const [selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-      { showDropdown ?
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        /> : null
-      }
+      {/*<button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>*/}
+      {/*{ showDropdown ?*/}
+        {/*<Dropdown*/}
+          {/*selected={selected}*/}
+          {/*onSelectedChange={setSelected}*/}
+          {/*options={options}*/}
+        {/*/> : null*/}
+      {/*}*/}
+      <Translate />
     </div>
   );
 };
---
+++ src/components/Translate.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import Dropdown from './Dropdown';
+
+const options = [
+  {label: 'Africaans', value: 'af'},
+  {label: 'Arabic', value: 'ar'},
+  {label: 'Hindi', value: 'hi'}
+];
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState('');
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter text</label>
+          <input
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+        </div>
+      </div>
+      <Dropdown
+        label="Select a language"
+        selected={language}
+        onSelection={setLanguage}
+        options={options}
+      />
+    </div>
+  )
+};
+
+export default Translate;
