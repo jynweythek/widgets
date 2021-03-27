af13f08da3a7798fc031f30e9d03c1101ad94ffb
Rustam Talapov <rustam_talapov@epam.com>
2021-03-27 12:10:20 +0200

    Dropdown component creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import Search from "./components/Search";
+import React, {useState} from 'react';
+// import Accordion from "./components/Accordion";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const App = () => {
+// const items = [
+//   {title: 'What is React?', content: 'React is a front end javascript framework'},
+//   {title: 'Why use React?', content: 'React is a favorite JS library among engineers'},
+//   {title: 'How do you use React?', content: 'You use React by creating components'}
+// ];
+
+const options = [
+  {label: 'The color Red', value: 'red'},
+  {label: 'The color Green', value: 'green'},
+  {label: 'A shade of Blue', value: 'blue'}
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      <Search />
+      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+      { showDropdown ?
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        /> : null
+      }
     </div>
   );
 };
-
-export default App
